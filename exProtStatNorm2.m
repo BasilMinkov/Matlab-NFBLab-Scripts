376e15d61305e17d0c9053b947ee205232119130
Basil Minkov <proveyourselfmail@gmail.com>
2016-11-14 15:26:14 +0300

    Script for 07.10.16 results
    Next step: 
    1) Make single protocol filtering inside of the permutation loop. 
    2) Compare the result of "next step" to "script for 07.10.16 results". 
    
    Later on: 
    1) "Plotting Some Stuff" has broken down. Should fix it. 
    2) "Normalization" is not considered properly yet.

--- exProtStatNorm2.m
+++ exProtStatNorm2.m
@@ -3,50 +3,60 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Getting permutated values
+% Getting real & permutated values for different frequencies
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% X{1} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol2'); 
-% X{2} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol8'); 
-% X{3} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol12');
-% X{4} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol14');
-% Y{1} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol4'); 
-% Y{2} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol6'); 
-% Y{3} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol10'); 
-% Y{4} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A12_d1', 'protocol16');
+% Reading all the protocols from the dataset. I'm planing to make a few
+% functions for each experimental design in order not to rewrite this part
+% for each particepent.
+% Xn and Yn ??protocols for different conditions. 
+% "Xd" ? "dirty" X dataset. "Yd" ? "dirty" Y dataset. 
 
-X{1} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol2'); 
-X{2} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol8'); 
-X{3} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol12');
-X{4} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol14');
-Y{1} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol4'); 
-Y{2} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol6'); 
-Y{3} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol10'); 
-Y{4} = hdf5read('D:\Minkov\Alpha Feedback\Data\One Day Data\A18_d1\raw.h5', 'protocol16');
+Xd{1} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol2'); 
+Xd{2} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol8'); 
+Xd{3} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol12');
+Xd{4} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol14');
+Yd{1} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol4'); 
+Yd{2} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol6'); 
+Yd{3} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol10'); 
+Yd{4} = hdf5read('/Users/basilminkov/Desktop/Neurofeedback/Data/A12/raw.h5', 'protocol16');
 
-h = waitbar(0, 'Wait...');
-steps = 100;
-srate = 500;
+% General Settings
 
-for f = 1:40
+steps = 300; % number of permutations
+srate = 500; % sampling rate of data
+h = waitbar(0, 'Wait...'); % initializing the process bar
+distDel = 500; % number of possible distorted values that should be droped
 
-    band=[f f+2];
-    order = 3;
-    [b]= fir1(90, band*2/srate);
+
+for frequency=1:40 % frequency for FIR filter
+
+    % Setting FIR filter 
+    band=[frequency frequency+2];
+    order = 600;
+    [b]= fir1(order, band*2/srate);
     a = [1];
+
+    % Appling FIR filter to two kinds of datasets 
+    
+    for i = 1:length(Xd)
+        x = filtfilt(b, a, Xd{i}')';
+        x(18,:) = []; % subject's special bad channel
+        X{i} = x(:, distDel:end-distDel); % getting rid of distortion
+        y = filtfilt(b, a, Yd{i}')';
+        y(18,:) = []; % subject's special bad channel
+        Y{i} = y(:, distDel:end-distDel); % getting rid of distortion
+    end 
+    
+    % Making permutations
     
     for step=1:steps
 
         ind = randperm(8);
         XY = {X{1} X{2} X{3} X{4} Y{1} Y{2} Y{3} Y{4}};
 
-        XXd = [XY{ind(1)} XY{ind(2)} XY{ind(3)} XY{ind(4)}];
-        YYd = [XY{ind(5)} XY{ind(6)} XY{ind(7)} XY{ind(8)}];
-        XXd(18,:) = []; % subject's special
-        YYd(18,:) = []; % subject's special
-
-        XX = filtfilt(b,a,XXd')';
-        YY = filtfilt(b,a,YYd')';
+        XX = [XY{ind(1)} XY{ind(2)} XY{ind(3)} XY{ind(4)}];
+        YY = [XY{ind(5)} XY{ind(6)} XY{ind(7)} XY{ind(8)}];
 
         l_XX = length(XX);
         l_YY = length(YY);
@@ -66,52 +76,50 @@ for f = 1:40
         % try different regularization parameters p_reg
 
         [V, d] = eig(C1,C2);
-        W{f, step} = inv(V');
-        % time_passed = toc
-
-        D_sg(step,:) = diag(d)';
+        D_sg(step,:) = diag(d)'; 
+        
+        % Getting a 3D matrix of components (x) for different frequencies
+        % for different permutations (z)
         
         for ii=1:size(d,2)
-           D(f,ii,step) = d(ii,ii);
+           D_perm(frequency,ii,step) = d(ii,ii);
         end
-    end %step
-   
-    D_f{f} = D_sg
+        
+    end
+
+    % Getting real eigenvalues for different frequencies
     
     ind = 1:8;
-    XY = {X{1} X{2} X{3} X{4} Y{1} Y{2} Y{3} Y{4}};
+    XYr = {X{1} X{2} X{3} X{4} Y{1} Y{2} Y{3} Y{4}};
 
-    Xd = [XY{ind(1)} XY{ind(2)} XY{ind(3)} XY{ind(4)}];
-    Yd = [XY{ind(5)} XY{ind(6)} XY{ind(7)} XY{ind(8)}];
-    Xd(18,:) = [];
-    Yd(18,:) = [];
-    XX = Xd;
-    YY = Yd;
-
-    l_X = length(XX);
-    l_Y = length(YY);
+    Xr = [XYr{ind(1)} XYr{ind(2)} XYr{ind(3)} XYr{ind(4)}];
+    Yr = [XYr{ind(5)} XYr{ind(6)} XYr{ind(7)} XYr{ind(8)}];
+    
+    l_Xr = length(Xr);
+    l_Yr = length(Yr);
 
     % Find covariances C10 and C20 
-    C10 = XX*XX'/l_X;
-    C20 = YY*YY'/l_Y;
+    C10r = Xr*Xr'/l_Xr;
+    C20r = Yr*Yr'/l_Yr;
 
-    nchan = size(C10,1);
+    nchan = size(C10r,1);
 
-    C10n = C10/trace(C10);
-    C20n = C20/trace(C20);
+    C10nr = C10r/trace(C10r);
+    C20nr = C20r/trace(C20r);
 
     % Tikhonov regularization
-    C1 = C10n + 0.1 * trace(C10n) * eye(nchan) / size(C10n,1); 
-    C2 = C20n + 0.1 * trace(C20n) * eye(nchan) / size(C20n,1);
+    C1r = C10nr + 0.1 * trace(C10nr) * eye(nchan) / size(C10nr,1); 
+    C2r = C20nr + 0.1 * trace(C20nr) * eye(nchan) / size(C20nr,1);
     % try different regularization parameters p_reg
 
-    % W0 - cells of matrixes of eigenvectors
-    % D0 - a matrix of eigenvalues: component (rows), frequency (column)
+    % Wr - cells of matrixes of eigenvectors
+    % Dr - a matrix of eigenvalues: component (rows), frequency (column)
     % FF - vector of frequencies
-    [Ve, de] = eig(C1,C2); 
-    De(f, :) = diag(de)';
-    W0{f}= inv(Ve');
-    FF(f) = f+1;
+    % ...r means "real"
+    [Ve, de] = eig(C1r,C2r); 
+    Dr(frequency, :) = diag(de)';
+    Wr{frequency}= inv(Ve');
+    FF(frequency) = frequency+1;
     
     for numCompStat=1:30
         
@@ -119,13 +127,15 @@ for f = 1:40
             permEVs(i) = D_sg(i, numCompStat);
         end
 
-        comp = De(f, numCompStat);
+        comp = Dr(frequency, numCompStat);
         
-        p_value_lol(f, numCompStat) = 1 - ((1*comp)/max(permEVs));
-        p_value(f, numCompStat) = (length(find(permEVs>comp))/length(permEVs));
-        
+        p_valuePlus(frequency, numCompStat) = (length(find(permEVs>comp))/length(permEVs));
+        p_valueMinus(frequency, numCompStat) = (length(find(permEVs<comp))/length(permEVs));
+
     end
-    waitbar((f/40)) 
+    
+    waitbar((frequency/40)) 
+    
 end
 
 close(h);
@@ -136,73 +146,76 @@ close(h);
 
 % Make Vector Of Maximum
 
-for step = 1:steps
-    DmxDistr(step) = max(max(D(:,:,step)));
-end;
+% for step = 1:steps
+%     DmxDistr(step) = max(max(D(:,:,step)));
+% end;
 
 % Make New P-Value
 
-for f= 1:40
-    for numCompStat=1:30
-        comp = De(f, numCompStat);
-        p_value_norm(f, numCompStat) = length(find(DmxDistr > comp)) / steps;
-    end
-end
+% for f= 1:40
+%     for numCompStat=1:30
+%         comp = De(f, numCompStat);
+%         p_value_norm(f, numCompStat) = length(find(DmxDistr > comp)) / steps;
+%     end
+% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Plotting Some Stuff 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-p = p_value;
+p = p_valueMinus;
 
 % Setting topoplotting properties
 chanlocs_vis = makeChanlocsVis();
 
-H = uicontrol('Style', 'PushButton', ...
-                    'String', 'Break', ...
-                    'Callback', 'delete(gcbf)');         
+% H = uicontrol('Style', 'PushButton', ...
+%                     'String', 'Break', ...
+%                     'Callback', 'delete(gcbf)');         
     
 % Plot "component's eigenvalue = f(frequency)"
 
-f1 = figure(1);
+figure(1);
 % plot(FF, D0(:, numComp));
 imagesc(p);
 colorbar;
 
-while (ishandle(H))
-    
-    f1;
-    
-    % Picking figure's coordinates
-    [x0,y0] = ginput(1);
-    x = round(x0);
-    y = round(y0);
-    
-    if x > length(W0)
-        continue
-    end
-    
-    if x < 0
-        continue
-    end
-    
-    if y > length(W0)
-        continue
-    end
-    
-    if y < 0
-        continue
-    end
-    
-    fprintf('Fq: %d', y)
-    fprintf('Comp: %d', x)
-    
-    figure(2);
-    clf();
-    topoplot(W0{y}(:, x), chanlocs_vis, 'electrodes', 'on');
-    title(num2str(De(y, x)));
-    
-end
+% while (ishandle(H))
+%     
+%     figure(1);
+%     
+%     % Picking figure's coordinates
+%     [x0,y0] = ginput(1);
+%     x = round(x0);
+%     y = round(y0);
+%     
+%     if x > length(W0)
+%         continue
+%     end
+%     
+%     if x < 0
+%         continue
+%     end
+%     
+%     if y > length(W0)
+%         continue
+%     end
+%     
+%     if y < 0
+%         continue
+%     end
+%     
+%     fprintf('Fq: %d', y)
+%     fprintf('Comp: %d', x)
+%     
+%     figure(2);
+%     clf();
+%     topoplot(W0{y}(:, x), chanlocs_vis, 'electrodes', 'on');
+%     title(num2str(De(y, x)));
+%     
+% end
 
-saveas(f1, 'D:\Minkov\Alpha Feedback\Analysis\A18_d1\A18_d1_p_value.png')
-save('D:\Minkov\Alpha Feedback\Analysis\A18_d1\A18_d1.mat')
+% save('/Users/basilminkov/Desktop/Neurofeedback/Analysis/A12/A12_explicit/A12_explicit_p_value_lol', p_value_lol);
+% save('/Users/basilminkov/Desktop/Neurofeedback/Analysis/A12/A12_explicit/A12_explicit_p_value', p_value);
+% save('/Users/basilminkov/Desktop/Neurofeedback/Analysis/A12/A12_explicit/A12_explicit_W0', W0);
+% save('/Users/basilminkov/Desktop/Neurofeedback/Analysis/A12/A12_explicit/A12_explicit_De', De);
+% saveas('/Users/basilminkov/Desktop/Neurofeedback/Analysis/A12/A12_explicit/A12_explicit_comp=f(Fr)', figure(1));
